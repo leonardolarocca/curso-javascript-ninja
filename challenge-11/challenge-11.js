c20aa438db2e5cf0b25744119bb6cc1331883ea8
leonardolarocca <leonardo_larocca@hotmail.com>
2018-04-24 22:45:50 -0300

    adicionando alguns exercicios

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,5 +1,5 @@
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
-
+(() => {
 /*
 Crie uma variável chamada `once`, que recebe `false` como seu valor.
 Crie um loop que execute enquanto essa variável for verdadeira. Dentro do
@@ -7,14 +7,22 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
-// ?
+let once = false;
+do {
+  console.log(`Entrou ao menos uma vez!`)
+} while (once);
 
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
-// ?
+let person = {
+  name: `Leonardo`,
+  age: 22,
+  weight: 83,
+  birthday: `12/06/1995`
+};
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -25,7 +33,9 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
-// ?
+for (let prop in person) {
+  console.log(`The ${prop} of person is ${person[prop]}`);
+}
 
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
@@ -34,21 +44,37 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
-// ?
+function moreThan (age) {
+ return age > person.age ? true : false;
+}
+console.log(`The person has more than 25 years old? ${moreThan(35)}`)
 
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
 array chamado `numbers`. Se o contador for maior que 10, saia do loop.
 Mostre no console os números no array.
 */
-console.log( 'De 0 a 10:' );
-// ?
-
+let numbers = [], cont = 0;
+while (cont <= 20) {
+  if (cont > 10 ){
+    break;
+  }
+  numbers.push(cont++);
+}
+console.log(`De 0 a 10: ${numbers}` );
 /*
 Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
 criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
 esses. Se o número for ímpar, pular para o próximo número.
 Mostrar no console os números do array.
 */
-console.log( 'Pares de 0 a 20:' );
-// ?
+numbers = [], cont = 0;
+while (cont <= 20) {
+  if (cont % 2) {
+    cont++;
+    continue;
+  }
+  numbers.push(cont++);
+}
+console.log(`Pares de 0 a 20: ${numbers}`);
+})();
--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,100 +1,105 @@
 /*
 Envolva todo o conteúdo desse arquivo em uma IIFE.
 */
+(() => {
+  /*
+  Crie um objeto chamado `person`, com as propriedades:
+  `name`: String
+  `lastname`: String
+  `age`: Number
+  Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
+  de valor para cada propriedade.
+  */
+  let person = {
+    name: `Leonardo`,
+    lastname: `Larocca`,
+    age: 22
+  };
+  console.log(`Propriedades de "person":` );
 
-/*
-Crie um objeto chamado `person`, com as propriedades:
-    `name`: String
-    `lastname`: String
-    `age`: Number
-Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
-de valor para cada propriedade.
-*/
-// ?
-console.log( 'Propriedades de "person":' );
+  /*
+  Mostre no console, em um array, todas as propriedades do objeto acima.
+  Não use nenhuma estrutura de repetição, nem crie o array manualmente.
+  */
+  console.log(Object.keys(person));
 
-/*
-Mostre no console, em um array, todas as propriedades do objeto acima.
-Não use nenhuma estrutura de repetição, nem crie o array manualmente.
-*/
-// ?
+  /*
+  Crie um array vazio chamado `books`.
+  */
+ let books = [];
 
-/*
-Crie um array vazio chamado `books`.
-*/
-// ?
+  /*
+  Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
+  seguintes propriedades:
+  `name`: String
+  `pages`: Number
+  */
+  books.push({ name: `Código Limpo`, pages: 100 });
+  books.push({ name: `Algoritmos`, pages: 300 });
+  books.push({ name: `Javascript`, pages: 250 });
 
-/*
-Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
-seguintes propriedades:
-`name`: String
-`pages`: Number
-*/
-// ?
-console.log( '\nLista de livros:' );
+  console.log( `Lista de livros: ` );
 
-/*
-Mostre no console todos os livros.
-*/
-// ?
+  /*
+  Mostre no console todos os livros.
+  */
+  console.log(`Books: ${JSON.stringify(books, null, ' ')}`);
 
-console.log( '\nLivro que está sendo removido:' );
-/*
-Remova o último livro, e mostre-o no console.
-*/
-// ?
+  /*
+  Remova o último livro, e mostre-o no console.
+  */
+  console.log( `Livro que está sendo removido: ${JSON.stringify(books.pop())}` );
 
-console.log( '\nAgora sobraram somente os livros:' );
-/*
-Mostre no console os livros restantes.
-*/
-// ?
 
-/*
-Converta os objetos que ficaram em `books` para strings.
-*/
-// ?
-console.log( '\nLivros em formato string:' );
+  /*
+  Mostre no console os livros restantes.
+  */
+ console.log( `Agora sobraram somente os livros: ${JSON.stringify((books), null, ' ')}` );
 
-/*
-Mostre os livros nesse formato no console:
-*/
-// ?
+  /*
+  Converta os objetos que ficaram em `books` para strings.
+  */
+  // ?
+  let new_books = JSON.stringify(books);
 
-/*
-Converta os livros novamente para objeto.
-*/
-// ?
-console.log( '\nAgora os livros são objetos novamente:' );
+  /*
+  Mostre os livros nesse formato no console:
+  */
+  console.log( `Livros em formato string: ${new_books}` );
 
-/*
-Mostre no console todas as propriedades e valores de todos os livros,
-no formato abaixo:
-    "[PROPRIEDADE]: [VALOR]"
-*/
-// ?
+  /*
+  Converta os livros novamente para objeto.
+  */
+  let obj_books = JSON.parse(new_books)
+  console.log( '\nAgora os livros são objetos novamente: ', obj_books );
 
-/*
-Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
-seu nome. Adicione seu nome completo no array.
-*/
-// ?
-console.log( '\nMeu nome é:' );
+  /*
+  Mostre no console todas as propriedades e valores de todos os livros,
+  no formato abaixo:
+  "[PROPRIEDADE]: [VALOR]"
+  */
+  for (var i = 0; i < books.length; i++) {
+    for (var prop in books[i]) {
+      console.log(prop + ': '+ books[i][prop]);
+    }
+  }
 
-/*
-Juntando todos os itens do array, mostre no console seu nome.
-*/
-// ?
+  /*
+  Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
+  seu nome. Adicione seu nome completo no array.
+  */
+  let myName = [`L`, `e`, `o`, `n`, `a`, `r`, `d`, `o`]
 
-console.log( '\nMeu nome invertido é:' );
-
-/*
-Ainda usando o objeto acima, mostre no console seu nome invertido.
-*/
-// ?
-
-console.log( '\nAgora em ordem alfabética:' );
-/*
-Mostre todos os itens do array acima, odenados alfabéticamente.
-*/
-// ?
+  /*
+  Juntando todos os itens do array, mostre no console seu nome.
+  */
+  console.log(`Meu nome é: `,myName.join(''))
+  /*
+  Ainda usando o objeto acima, mostre no console seu nome invertido.
+  */
+  console.log( `Meu nome invertido é: `,myName.reverse().join('') );
+  /*
+  Mostre todos os itens do array acima, odenados alfabéticamente.
+  */
+  console.log( `Agora em ordem alfabética: `, myName.sort().join('') );
+})();
